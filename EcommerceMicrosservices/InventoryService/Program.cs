9ee0ea415c1e5530445406a4d6014d350e7f14ea
Jess <jessica.sileite@gmail.com>
2025-08-21 15:50:16 -0300

    connections and dependencies

--- EcommerceMicrosservices/InventoryService/Program.cs
+++ EcommerceMicrosservices/InventoryService/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+   
+builder.Services.AddDbContext<OrganizadorContext>(options =>
+            options.UseMySql(
+                builder.Configuration.GetConnectionString("DefaultConnection"),
+                ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+            )
+        );
+
+// Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
